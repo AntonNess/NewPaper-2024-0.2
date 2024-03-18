90c6ebe71e7dbead6fb9d798c1bc8833e25d71e3
AntonNess <anton1gav@gmail.com>
2024-03-18 15:59:21 +0600

    Win
    Win

--- NewPaper/news/signals.py
+++ NewPaper/news/signals.py
@@ -1,18 +1,18 @@
 from django.contrib.auth.models import User
 from django.core.mail import EmailMultiAlternatives
-from django.db.models.signals import post_save
+from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 
-from .models import Post
+from .models import Post, PostCategory
 
 
-@receiver(post_save, sender=Post)
-def post_created(instance, created, **kwargs):
-    if not created:
+@receiver(m2m_changed, sender=PostCategory)
+def post_created(instance, **kwargs):
+    if not kwargs['action'] == 'post_add':
         return
 
     emails = User.objects.filter(
-        subscriptions__category=instance.postCategory
+        subscriptions__category__in=instance.postCategory.all()
     ).values_list('email', flat=True)
 
     subject = f'Новая статья в категории {instance.postCategory}'
@@ -25,7 +25,7 @@ def post_created(instance, created, **kwargs):
     html_content = (
         f'Заголовок: {instance.title}<br>'
         f'Текст: {instance.text}<br><br>'
-        f'<a href="http://127.0.0.1{instance.get_absolute_url()}">'
+        f'<a href="http://127.0.0.1:8000{instance.get_absolute_url()}">'
         f'Ссылка на статью</a>'
     )
     for email in emails:
